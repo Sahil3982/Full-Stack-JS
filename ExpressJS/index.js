b5643dea3350954245d566dd5e92d31a6e58ac51
sahil3982 <113926133+Sahil3982@users.noreply.github.com>
2024-01-31 01:46:41 +0530

    Jan 31

---
+++ ExpressJS/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+
+app.get('/', function(req,res){
+    res.send("Hello World");
+
+})
+
+app.get('/Home', (req,res)=>{
+    const obj = {
+        name:"sahil",
+        age: 18,
+        rollno: 45
+    }
+    res.send(obj);
+})
+
+app.get('/About',(req,res)=>{
+    res.send("This is About Page");
+})
+app.listen(3000);
