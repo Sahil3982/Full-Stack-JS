9871279a754fa4759de751c278e2ea89d56ea0cd
sahil3982 <113926133+Sahil3982@users.noreply.github.com>
2024-02-26 13:02:37 +0530

    Feb'26

---
+++ ContextAPI/src/App.jsx
@@ -0,0 +1,14 @@
+
+import UserContextProvider from '../context/UserContextProvider'
+import './App.css'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      
+    </UserContextProvider>
+  )
+}
+
+export default App
