986294a7e15cf004155d4b12a2f5d559810aaf9e
Sahil3982 <sahilpatel3982@gmail.com>
2024-05-21 16:55:25 +0530

    Done post req

--- PiyushNodeJS/RESTAPI/index.js
+++ PiyushNodeJS/RESTAPI/index.js
@@ -3,18 +3,19 @@ import userData from "./data.js";
 
 const port = 2000;
 const app = express();
-app.use( express.json())
+app.use(express.json());
 
-// app.use((req,res,next)=>{
-// console.log("Middleware 1st");
-// next();
-// })
-
-// app.use((req,res,next)=>{
-//   console.log("Middleware 2nd");
-//   return res.json({ message : "Route 1"})
+// Uncomment these middlewares if you need them for debugging or specific processing
+// app.use((req, res, next) => {
+//   console.log("Middleware 1st");
 //   next();
-// })
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Middleware 2nd");
+//   return res.json({ message: "Route 1" });
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -26,10 +27,10 @@ app.get("/api/user", (req, res) => {
 
 app.get("/user", (req, res) => {
   const html = `
-   <ul>
-   ${userData.map((user) => `<li>${user.first_name}</li>`).join("")}
-   </ul>
-   `;
+    <ul>
+      ${userData.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+  `;
   return res.send(html);
 });
 
@@ -37,22 +38,32 @@ app.get("/api/user/:id", (req, res) => {
   const id = Number(req.params.id);
   console.log(id);
 
-  const html2 = userData.find((user) => id === user.id);
-  res.json(html2);
+  const user = userData.find((user) => id === user.id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
 });
 
 app
-  .route("/api/user/:id")
+  .route("/api/user")
   .post((req, res) => {
-    const reqBody = req.body;
-    console.log('Res', reqBody);
-    return res.json({status : 'pending'})
+    const newUser = req.body;
+    console.log('Received new user data:', newUser);
+    
+    // You can add logic to save the new user data to your database here
+    // For example, you could push the new user to the userData array
+    
+    // res.status(201).json({ message: 'User created successfully', user: newUser });
   })
   .patch((req, res) => {
     console.log("PATCH Request");
+    // Add your patch logic here
   })
   .delete((req, res) => {
     console.log("Delete Request");
+    // Add your delete logic here
   });
 
 app.listen(port, () => {
