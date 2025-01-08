8a831be60ea22d54cf3eafa74f00e4fbc2d108e9
Sahil3982 <sahilpatel3982@gmail.com>
2025-01-09 00:32:42 +0530

    cors and cookieparser installed

--- backendExpress/src/index.js
+++ backendExpress/src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
-
+import {app} from "./app.js";
+import cors from "cors"
+import cookieParser from "cookie-parser"
 dotenv.config({
   path: './env'
 })
+app.use(cors())
 
 connectDB()
+.then(
+  app.listen(process.env.PORT || 8001 , ()=>{
+    console.log(`Port is runing on ${process.env.PORT || 8001 } `);
+    
+  })
+)
+.catch((error) => {
+  console.log("MONGODB Connection is Failed", error);
+  
+})
