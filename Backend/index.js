ebcd80e8528b8cfa6d5ccbf6dfaeb7aca82eb8c4
Sahil3982 <sahilpatel3982@gmail.com>
2025-06-25 23:10:04 +0530

    added some package

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
-import dbConnection from './db/db.js';
-const app = express();
+import dbConnection from './src/db/db.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 const PORT = process.env.PORT || 3000; 
+const app = express();
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
 
 dbConnection()
 
